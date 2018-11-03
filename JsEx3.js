4a448d78257f368ad9ae8c801675202c80f91cc8
= <=>
2018-11-03 15:27:48 +0700

    First

---
+++ JsEx3.js
@@ -0,0 +1,12 @@
+var a;
+var b;
+var c;
+a = prompt("nhap so thu nhat");
+b = prompt("nhap so thu hai");
+c = a % b;
+if (c == 0){
+    alert("a chia het cho b");
+}
+else   {
+    alert("a khong chia het cho b");
+}
